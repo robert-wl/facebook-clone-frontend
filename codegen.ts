2f8ed8bdade68a1f5e5a120ac531514bc1469163
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-08-06 09:25:55 +0700

    fix: typescript tsconfig.json error

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,6 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  //@ts-ignore
   schema: import.meta.env.VITE_GRAPHQL_BACKED_URL,
   documents: "*/**/*.graphql.ts",
   generates: {
